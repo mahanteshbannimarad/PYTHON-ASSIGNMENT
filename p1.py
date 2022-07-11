a31f2732adf6d80188d4c8d92b874f141ebfa493
mahanteshb <mahantesh2010@gmail.com>
2022-07-11 06:22:10 -0700

    commit

---
+++ p1.py
@@ -0,0 +1,10 @@
+def sum(a,b):
+    s=a+b
+    print(f"sum of two nums=",s)
+def prod(a,b):
+    p=a*b
+    print(f"Product of two nums=",p)
+a=int(input("Enter first num"));
+b=int(input("Enter second num"));
+sum(a,b)
+prod(a,b)
